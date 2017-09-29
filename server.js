8881cc2e28c59e6396da27bbd789765675022247
TheKriken <luis.enrique.alejandro@hotmail.com>
2017-09-29 07:39:17 -0500

    Adds default port to 0.0.0.0, Adds local run script

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 'use strict';
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-const express = require('express'),
+var express = require('express'),
       app = express(),
       pug = require('pug');
 
-    var html = pug.renderFile(__dirname + '/app/html/index.pug');
+var html = pug.renderFile(__dirname + '/app/html/index.pug');
 
-    app.use(express.static('public'));
+app.use(express.static('public'));
 
-    app.get('/', function (req, res) {
-        res.end(html)    
+app.get('/', function (req, res) {
+    res.end(html)    
+});
+
+app.get('*', function(req, res){
+    res.end(html) 
     });
 
-    app.get('*', function(req, res){
-        res.end(html) 
-      });
+app.listen(port, ip, function () {
+    console.log( "Listening on " + ip + ", server_port " + port  );    
+});
 
-    app.listen(server_port, server_ip_address, function () {
-        console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );    
-    });
+module.exports = app ;
