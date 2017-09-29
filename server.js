d6756783240c56cfb81b91c1dc4b2233d056906c
TheKriken <luis.enrique.alejandro@hotmail.com>
2017-09-28 23:41:04 -0500

    Adds openshift configs Changes app.js to server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 const express = require('express'),
       app = express(),
       pug = require('pug');
@@ -16,6 +19,6 @@ const express = require('express'),
         res.end(html) 
       });
 
-    app.listen(8000, function () {
-        console.log("Server running at http://127.0.0.1:8000/");    
+    app.listen(server_port, server_ip_address, function () {
+        console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );    
     });
